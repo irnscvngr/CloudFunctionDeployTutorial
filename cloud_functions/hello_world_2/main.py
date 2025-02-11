298618382de1fc7319f47df0ff8103f07352000b
irnscvngr <patrickhausmann@gmx.de>
2025-02-11 16:05:09 +0100

    made a mistake in previous commit. the function name needs to be "hello_world_function_2" inside the source code to reflect the name of the cloud_function

--- cloud_functions/hello_world_2/main.py
+++ cloud_functions/hello_world_2/main.py
@@ -3,5 +3,5 @@ import functions_framework
 # This is just a second hello_world_function to test:
 # can a new cloud-function be created directly by deploying from GitHub
 @functions_framework.http
-def hello_world_2(request):  # This is your Cloud Function
+def hello_world_function_2(request):  # This is your Cloud Function
     return "Hello World! This function was directly created in GitHub.", 200
